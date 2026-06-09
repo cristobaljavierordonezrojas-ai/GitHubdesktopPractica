364d4dfcbab84b43742b649f058facbc16df0174
tobalor1990 <tobalor1990@gmail.com>
2026-06-08 18:08:00 -0600

    Update ÁreaRectangulo.py
    Finalización del proyecto cálculo área del rectángulo

---
+++ Lenguajes de programación/ÁreaRectangulo.py
@@ -0,0 +1,5 @@
+print("Determine el Área de un Rectángulo\n")
+base=float(input("¿Cuántos centimetros mide la base del Rectángulo? "))
+altura=float(input("¿Cuántos centimetros mide la altura del Rectángulo? "))
+área=base*altura
+print(f"El área del Rectángulo mide {área:.2f} centimetros")
